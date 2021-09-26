3656d264ffae030338bacad83e66934dcb862552
Hamilton Marobane <hmaputla181@gmail.com>
2021-09-26 13:42:11 +0200

    Early Stages 0.0

--- ATM/src/FrontUser.java
+++ ATM/src/FrontUser.java
@@ -4,21 +4,23 @@ import com.user.User;
 
 public class FrontUser
 {
-	
-	
+
 	public static void main(String[] args)
 	{
 		//Declare List of Objects
 		ArrayList<User>listOfUsers=new ArrayList<User>();
 		
-		System.out.println("\n"+listOfUsers.size()+" added in the List ");
+		System.out.println(""+listOfUsers.size()+" added in the List ");
 		
 		RegisterUser(listOfUsers);
 		
 		System.out.println("\n"+listOfUsers.size()+" Added on the List");
 		
+		//ValidateAndLogin(listOfUsers);
 		
-		
+		//User userObj=new User();
+		//System.out.println("Your username is username "+userObj.getUserName()+" Last Name "+userObj.getLastName());
+
 	}//End of Main Class
 	
 	
@@ -88,6 +90,7 @@ public class FrontUser
 				message="Pin number must be 4 numerical values";
 				System.out.println(message);
 				terminal='Y';
+				listOfUsers.clear();
 				continue;
 			}
 			
@@ -97,26 +100,28 @@ public class FrontUser
 				System.out.println(message);
 				terminal='Y';
 				
-				message="\nRe-Enter User Details\n\n";
+				message="\nRe-Enter User Details\n";
 				System.out.println(message);
+				listOfUsers.clear();
 				continue;
 			}
 			if(!checkEmpty && pinLength && accountLength)
 			{
-				if( validatePin(password) && validateAccount(account))
+				if(ValidateData(password,account))
 				{
 					message="Successfully Registered with ATM";
 					System.out.println(message);
 					terminal='N';
 					continue;
 				}
-				else
+				if(!ValidateData(password,account))
 				{
 					message="Please Enter a valid pin/account";
 					System.out.println(message);
-					message="\nRe-Enter User Details\n\n";
+					message="\nRe-Enter User Details\n";
 					System.out.println(message);
 					terminal='Y';
+					listOfUsers.clear();
 					continue;
 				}
 			}
@@ -132,7 +137,172 @@ public class FrontUser
 	}
 
 	
-	public static boolean validatePin(String _password)
+	public static boolean ValidateData(String _password,String _account)
+	{
+			boolean results=false;
+				
+			char []pinNum=_password.toCharArray();
+			int countPin=0;
+			
+			char []accountNum=_account.toCharArray();
+			int countAccount=0;
+				
+			for(int outer=0;outer<_password.length();outer++)
+			{
+				if(Character.isDigit(pinNum[outer]))
+				{
+					countPin++;
+				}
+				
+			}
+				
+			
+			for(int outer=0;outer<_account.length();outer++)
+			{
+				if(Character.isDigit(accountNum[outer]))
+				{
+					countAccount++;
+				}
+			
+			}
+			
+			
+			if(countPin==4 && countAccount==10)
+			{
+				results=true;
+				
+			}
+
+			return results;
+	}
+	
+
+	public static boolean Login(ArrayList<User>listOfUsers,String account,String _pin)
+	{
+		boolean results=false;
+		
+		for(User each:listOfUsers)
+		{
+			if(each.getAccountNum().equalsIgnoreCase(account) && each.getPassword().equals(_pin))
+			{
+				results=true;
+				break;
+			}
+			else
+			{
+				results=false;
+				break;
+			}
+		}
+		
+		return results;
+	}
+	
+	 
+	public static boolean ValidateAndLogin(ArrayList<User>listOfusers)
+	{
+		boolean results=false;
+		String message="\n*****Login Into Your Account*****\n";
+		System.out.println(message);
+		
+		Scanner inputString=new Scanner(System.in);
+		//Scanner inputInt=new Scanner(System.in);
+
+		int counter=0;
+		
+		do
+		{
+			counter++;
+			
+			message="Enter Your Acccount number ";
+			System.out.println(message);
+			String account=inputString.nextLine();
+			
+			message="Enter Your Pin ";
+			System.out.println(message);
+			String pin=inputString.nextLine();
+			
+			
+			System.out.println("\nNumber of Attempts "+counter+"\n");
+			if(ValidateData(pin,account))
+			{
+	
+				 if(Login(listOfusers,account,pin))
+				 {
+					 results=true;
+					 counter=3;
+					 continue;
+				 }
+				 else
+				 {
+					 message="\nAccount/Pin do not Match,Try Again\n";
+					 System.out.println(message);
+				 }
+
+			}
+			else
+			{
+				if(counter!=3)
+				{
+					message="Please Enter Correct account/pin";
+					System.out.println(message);
+				}
+				
+				if(counter==3)
+				{
+					message="Many Attempts,You are Blocked please contact the Office.Thank You";
+					System.out.print(message);
+					results=false;
+					continue;
+				}
+			}
+			
+			
+			
+		}while(counter!=3);
+		
+		
+		return results;
+	
+	}
+	
+	
+	public static void Menu(ArrayList<User>listOfUsers)
+	{
+		String message="";
+		Scanner inputInt=new Scanner(System.in);
+		if(ValidateAndLogin(listOfUsers))
+		{
+			message="\n1.Deposit \n2.Withdraw/3.Check Balance\n";
+			message+="Welcome Please Choose an action above";
+			int option=inputInt.nextInt();
+			
+			options(listOfUsers,option);
+		}
+	}
+
+
+	public static void options(ArrayList<User>listOfUsers,int option) 
+	{
+		String message="";
+		switch(option)
+		{
+		case 1:
+		{
+			message="How much would you like to deposit";
+			System.out.println(message);
+			
+			for(User each:listOfUsers)
+			{
+				
+			}
+		}
+		break;
+		}
+		
+	}
+
+	/*public static boolean validatePin(String _password)
 	{
 		boolean results=false;
 		
@@ -159,10 +329,9 @@ public class FrontUser
 		
 		return results;
 		
-	}
+	}*/
 	
-	
-	public static boolean validateAccount(String Account)
+	/*public static boolean validateAccount(String Account)
 	{
 		boolean results=false;
 		
@@ -188,31 +357,18 @@ public class FrontUser
 		}
 		return results;
 		
-	}
+	}*/
 	
-	
-	public static boolean Login(ArrayList<User>listOfUsers,String _username,String _pin)
+	/*public static boolean LoopMenu()
 	{
 		boolean results=false;
 		
-		for(User each:listOfUsers)
-		{
-			if(each.getUserName().equalsIgnoreCase(_username) && each.getPassword().equals(_pin))
-			{
-				results=true;
-				break;
-			}
-			else
-			{
-				results=false;
-				break;
-			}
-		}
+		while()
 		
 		return results;
-	}
+	}*/
 	
-	public static void Menu(User userObj)
+	/*public static void Menu(User userObj)
 	{
 		Scanner inputInt=new Scanner(System.in);
 		Scanner inputDouble=new Scanner(System.in);
@@ -242,7 +398,7 @@ public class FrontUser
 		}
 		
 		
-	}
+	}*/
 
 }
 
